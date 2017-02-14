a6d7be890fe5c4ae90933bdfa58d4586610a8732
Ito Atsushi <aito6e@gmail.com>
2017-02-15 08:38:15 +0900

    initial monte calro player

---
+++ tic-tac-toe/policy.py
@@ -0,0 +1,47 @@
+"""
+Policy functions
+"""
+
+import numpy as np
+import math
+
+NUM_ACTIONS = 9
+
+def can_take(state, action):
+    return ((3 << action * 2) & state) == 0
+
+class Greedy:
+
+    def execute(self, q, state):
+        policy = [0] * NUM_ACTIONS
+        pairs = [(i,v) for i, v in enumerate(q[state]) if can_take(state, i)]
+        policy[max(pairs, key=lambda x: x[1])[0]] = 1
+        return policy
+
+class Softmax:
+
+    def __init__(self, temperature):
+       self._temperature = temperature
+       
+    def execute(self, q, state):
+        policy = [0] * NUM_ACTIONS
+        pairs = [(i,v) for i, v in enumerate(q[state]) if can_take(state, i)]
+        for p in pairs:
+            policy[p[0]] = math.exp(p[1] / self._temperature)
+        policy_sum = sum(policy)
+        return [v / policy_sum for v in policy]
+    
+class EGreedy:
+
+    def __init__(self, epsilon):
+        self._epsilon = epsilon
+
+    def execute(self, q, state):
+        policy = [0] * NUM_ACTIONS
+        pairs = [(i,v) for i, v in enumerate(q[state]) if can_take(state, i)]
+        for p in pairs:
+            policy[p[0]] = self._epsilon / len(pairs)
+        policy[max(pairs, key=lambda x: x[1])[0]] += (1 - self._epsilon)
+        return policy
+              
+    
---
+++ tic-tac-toe/randomopponent.py
@@ -0,0 +1,55 @@
+"""
+Random Opponent
+"""
+
+import random
+
+class RandomOpponent:
+
+    NUM_ROWS = 3
+    NUM_COLS = 3
+    NUM_ACTIONS = NUM_ROWS * NUM_COLS
+
+    def move(self, state):
+        """
+        choose a next action randomly.
+        """
+        action = self.get_last_cell(state)
+        if action >= 0:
+            return action
+        candidates = [i for i in range(self.NUM_ACTIONS)
+                      if (3 << i * 2) & state == 0]
+        return self.random.choice(candidates)
+
+    def get_last_cell(self, state):
+        for i in range(self.NUM_ROWS):
+            row_state = (state & (63 << i * 6)) >> i * 6
+            if row_state == 5 or row_state == 10:
+                return 2 + i * 3
+            if row_state == 17 or row_state == 34:
+                return 1 + i * 3
+            if row_state == 20 or row_state == 40:
+                return i * 3
+            col_state = (state & 12483 << i * 2) >> i * 2
+            if col_state == 65 or col_state == 130:
+                return 6 + i
+            if col_state == 4097 or col_state == 8194:
+                return 3 + i
+            if col_state == 4160 or col_state == 8320:
+                return i
+        diag_state = state & 197379
+        if diag_state == 257 or diag_state == 514:
+           return 8
+        if diag_state == 65537 or diag_state == 65792:
+            return 4
+        if diag_state == 131074 or diag_state == 131584:
+            return 0
+        diag_state2 = state & 13104
+        if diag_state2 == 272 or diag_state2 == 544:
+            return 8
+        if diag_state2 == 4112 or diag_state2 == 4532:
+            return 4
+        if diag_state2 == 8224 or diag_state2 == 8704:
+            return 0
+        return -1
+               
