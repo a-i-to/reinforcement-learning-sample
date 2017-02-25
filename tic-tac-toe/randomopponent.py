822866b8d3bc09afdfc439dfe12baf61b2230efa
Ito Atsushi <aito6e@gmail.com>
2017-02-25 10:19:08 +0900

    use module variables

--- tic-tac-toe/randomopponent.py
+++ tic-tac-toe/randomopponent.py
@@ -4,11 +4,10 @@ Random Opponent
 
 import random
 
-class RandomOpponent:
+import game
 
-    NUM_ROWS = 3
-    NUM_COLS = 3
-    NUM_ACTIONS = NUM_ROWS * NUM_COLS
+
+class RandomOpponent:
 
     def move(self, state):
         """
@@ -17,12 +16,12 @@ class RandomOpponent:
         action = self.get_last_cell(state)
         if action >= 0:
             return action
-        candidates = [i for i in range(self.NUM_ACTIONS)
+        candidates = [i for i in range(game.NUM_CELLS)
                       if (3 << i * 2) & state == 0]
         return self.random.choice(candidates)
 
     def get_last_cell(self, state):
-        for i in range(self.NUM_ROWS):
+        for i in range(game.NUM_CELLS_IN_ROW):
             row_state = (state & (63 << i * 6)) >> i * 6
             if row_state == 10:
                 return 2 + i * 3
@@ -39,7 +38,7 @@ class RandomOpponent:
                 return i
         diag_state = state & 197379
         if diag_state == 514:
-           return 8
+            return 8
         if diag_state == 65792:
             return 4
         if diag_state == 131584:
@@ -52,7 +51,7 @@ class RandomOpponent:
         if diag_state2 == 8704:
             return 2
 
-        for i in range(self.NUM_ROWS):
+        for i in range(game.NUM_CELLS_IN_ROW):
             row_state = (state & (63 << i * 6)) >> i * 6
             if row_state == 5:
                 return 2 + i * 3
@@ -69,7 +68,7 @@ class RandomOpponent:
                 return i
         diag_state = state & 197379
         if diag_state == 257:
-           return 8
+            return 8
         if diag_state == 65537:
             return 4
         if diag_state == 131074:
